9b7d87f4d39fd7cd05ef8f153cf2f0e6382c9eb3
Owen Kz <lordkz19@gmail.com>
2026-04-22 23:31:09 +0100

    button fixed

--- src/components/dashboard/DashboardPortfolio.tsx
+++ src/components/dashboard/DashboardPortfolio.tsx
@@ -6,6 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { Button } from "../ui/button";
 
 const DashboardPortfolio = () => {
   const { get, post, put, del } = useApiClient();
